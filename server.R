1f0b51e0ae253bf8d2c433d568403da7e8aebf4a
helengyy <helengyy@hotmail.com>
2015-01-12 14:15:27 +0800

    Signed-off-by: helengyy <helengyy@hotmail.com>

--- server.R
+++ server.R
@@ -12,8 +12,8 @@ calBMI <- function(system, height, weight) {
 
 shinyServer(
   function(input, output) {
-    output$oHeight <- renderText({paste("Height:",input$height)})
-    output$oWeight <- renderText({paste("Weight:",input$weight)})
-    output$oBMI <- renderText({paste("BMI:", calBMI(input$system, input$height, input$weight))})
+    output$height <- renderPrint({input$height})
+    output$weight <- renderPrint({input$weight})
+    output$BMI <- renderPrint({calBMI(input$system, input$height, input$weight)})
   }
 )
--- ui.R
+++ ui.R
@@ -5,13 +5,17 @@ shinyUI(pageWithSidebar(
     radioButtons('system', h4('Select Measurements'), choices = list('Metric' = 1, 'Imperial' = 2),selected = 2),
     numericInput('height', h4('Height(cm/in)'), 0,min=0,max=250,step=1),
     numericInput('weight', h4('Weight(kg/lbs)'),0,min=0,max=250,step=1),
-    submitButton('Submit')
+    submitButton('Submit'),
+    a("help",href="help.html")
   ),
   mainPanel(
     h4('Result'),
-    textOutput("oHeight"),
-    textOutput("oWeight"),
-    h5(textOutput("oBMI")),
+    h5('Height'),
+    verbatimTextOutput("height"),
+    h5('Weight'),
+    verbatimTextOutput("weight"),
+    h5('BMI'),
+    h5(verbatimTextOutput("BMI")),
     br(),
     div(h4("BMI Classification"),h6("18.5 or less___ Underweight"),
         h6("18.5 to 24.99__ Normal Weight"),h6("25 to 29.99____ Overweight"),
